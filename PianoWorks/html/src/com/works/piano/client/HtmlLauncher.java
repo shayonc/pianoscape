405946542788a7b85c61f38c62186f263ed90d06
Matthew Ngai <mtfngai@edu.uwaterloo.ca>
2017-06-18 17:53:26 -0400

    libgdx project

---
+++ PianoWorks/html/src/com/works/piano/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.works.piano.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.works.piano.PianoWorks;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new PianoWorks();
+        }
+}
