58d0c8bed7bf5a931a2d866e801f9d714b391d95
Matthew Ngai <mtfngai@edu.uwaterloo.ca>
2017-06-19 13:38:36 -0400

    Added classes for models

---
+++ PianoTrainer/app/src/main/java/piano/pianotrainer/model/Identity.java
@@ -0,0 +1,8 @@
+package piano.pianotrainer.model;
+
+/**
+ * Created by Matthew on 6/19/2017.
+ */
+
+public class Identity {
+}
---
+++ PianoTrainer/app/src/main/java/piano/pianotrainer/model/Note.java
@@ -0,0 +1,9 @@
+package piano.pianotrainer.model;
+
+/**
+ * Created by Matthew on 6/19/2017.
+ */
+
+public class Note {
+
+}
