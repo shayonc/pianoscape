cdb2f1c6cf364d75c9115a08f5981523d13cbbba
Matthew Ngai <mtfngai@edu.uwaterloo.ca>
2017-06-20 14:39:26 -0400

    input stream

--- PianoTrainer/app/src/main/java/piano/pianotrainer/parser/XMLMusicParser.java
+++ PianoTrainer/app/src/main/java/piano/pianotrainer/parser/XMLMusicParser.java
@@ -1,9 +1,18 @@
 package piano.pianotrainer.parser;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by Matthew on 6/18/2017.
  */
 
 public class XMLMusicParser {
+    public XMLMusicParser(String filename) throws IOException {
+        File input = new File(filename);
 
+        InputStream is = new FileInputStream(filename);
+    }
 }
