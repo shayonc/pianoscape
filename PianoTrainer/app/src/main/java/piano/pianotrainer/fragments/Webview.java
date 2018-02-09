b1dcc916955aa0e652b1f4cf7c9445a609219fef
Matthew Ngai <mtfngai@edu.uwaterloo.ca>
2018-02-09 17:10:38 -0500

    webview

---
+++ PianoTrainer/app/src/main/java/piano/pianotrainer/fragments/Webview.java
@@ -0,0 +1,59 @@
+package piano.pianotrainer.fragments;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import piano.pianotrainer.R;
+
+public class Webview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccessFromFileURLs(true); //Maybe you don't need this rule
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+//        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+//        myWebView.loadUrl("www.google.com");
+//        myWebView.setWebChromeClient(new WebChromeClient());
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("file://")) {
+                    // magic
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        myWebView.loadUrl("file:///android_asset/index.html");
+
+
+
+    }
+
+}
