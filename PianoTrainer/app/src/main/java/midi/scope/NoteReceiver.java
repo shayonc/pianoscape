edbe081face94b462a12c2e45dc4cfa3a4acb80c
Matthew Ngai <mtfngai@edu.uwaterloo.ca>
2018-03-07 18:17:26 -0500

    Revert "code cleanup"
    This reverts commit 5a4240a80b9ff56cea9fa3438c873a207b2a5c12.

--- PianoTrainer/app/src/main/java/midi/scope/NoteReceiver.java
+++ PianoTrainer/app/src/main/java/midi/scope/NoteReceiver.java
@@ -17,13 +17,20 @@
 package midi.scope;
 
 import android.media.midi.MidiReceiver;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.locks.Lock;
 
+import piano.pianotrainer.fragments.MusicDialogFragment;
 import piano.pianotrainer.model.ChordComparator;
 import piano.pianotrainer.model.Note;
 import uk.co.dolphin_com.seescoreandroid.MainActivity;
@@ -44,6 +51,7 @@ public class NoteReceiver extends MidiReceiver {
     private ArrayList<Integer> correctList = new ArrayList<>();
     private Lock compLock;
     private int curNote;
+    private int incorrectCount;
     private int restCount = 0;
     private ChordComparator chordComparator;
     private boolean isChord = false;
@@ -181,6 +189,7 @@ public class NoteReceiver extends MidiReceiver {
                     sb.append("\nNote " + curNote + " was incorrect\n");
                     sb.append("Expected octave " + expNote.midiPitch / 12 + " and step " + expNote.midiPitch % 12 + "\n");
                     sb.append("Given octave " + note.getOctave() + " and step " + note.getStep() + "\n");
+                    //incorrectCount++;
                 }
             }
             else {
@@ -215,7 +224,7 @@ public class NoteReceiver extends MidiReceiver {
             Log.d("NoteReceiverEnd", "Last note of song");
             lastNote = true;
         }
-        else if(curNote == 100 || curNote >= notes.size() ){
+        else if(curNote == 100 || curNote >= notes.size()/* notes.size()*/){
             //end song
             songOver = true;
             Log.d("NoteReceiverEnd", "Should launch summary activity");
